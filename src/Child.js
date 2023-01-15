6de9d676fd534393dfdf727a3a581d279d60352f
Marcus <marcus.kariuki@student.moringaschool.com>
2023-01-15 23:54:04 +0300

    assignment completed

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child(props) {
+
+  return <div
+    onClick={()=> props.onChangeColor(getRandomColor)} 
+    className="child" style={{ backgroundColor: props.color }} />;
 }
 
 export default Child;
